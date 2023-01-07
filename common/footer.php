bdfb48d3f8a6bbbf55f006ee34397cc2cf67e884
Olena ILCHENKO <olena.ilchenko@etud.gretacfa-montpellier.fr>
2023-01-07 11:07:42 +0100

    ajout projet integration_php

--- common/footer.php
+++ common/footer.php
@@ -1,3 +1,3 @@
 <footer class="footer text-faded text-center py-5">
-    <div class="container"><p class="m-0 small">Copyright &copy; Your Website 2023 Olena ILCHENKO</p></div>
+    <div class="container"><p class="m-0 small">Copyright &copy; Your Website 2023 Olena Ilchenko</p></div>
 </footer>
